ef26cbf9d9c3563833a5c464d5f01816add5b229
Pradeepraj.R <Pradeepraj.R@infosys.com>
2017-11-05 00:46:29 +0530

    Added URLs to cards

--- src/app/constants/crypto-constants.ts
+++ src/app/constants/crypto-constants.ts
@@ -1,13 +1,13 @@
 
 export const CRYPTO_DATA = [
-  { name: "Bitcoin", api: "https://api.coinmarketcap.com/v1/ticker/bitcoin/" },
-  { name: "Ethereum", api: "https://api.coinmarketcap.com/v1/ticker/ethereum/" },
-  { name: "Bitcoin Cash", api: "https://api.coinmarketcap.com/v1/ticker/bitcoin-cash/" },
-  { name: "Ripple", api: "https://api.coinmarketcap.com/v1/ticker/ripple/" },
-  { name: "Litecoin", api: "https://api.coinmarketcap.com/v1/ticker/litecoin/" },
-  { name: "Dash", api: "https://api.coinmarketcap.com/v1/ticker/dash/" },
-  { name: "NEO", api: "https://api.coinmarketcap.com/v1/ticker/neo/" },
-  { name: "NEM", api: "https://api.coinmarketcap.com/v1/ticker/nem/" },
-  { name: "Monero", api: "https://api.coinmarketcap.com/v1/ticker/monero/" },
-  { name: "Ethereum Classic", api: "https://api.coinmarketcap.com/v1/ticker/ethereum-classic/" }
+  { name: "Bitcoin", api: "https://api.coinmarketcap.com/v1/ticker/bitcoin/", site: "https://coinmarketcap.com/currencies/bitcoin/", className: "s-s-bitcoin crypto-logo" },
+  { name: "Ethereum", api: "https://api.coinmarketcap.com/v1/ticker/ethereum/", site: "https://coinmarketcap.com/currencies/ethereum/", className: "s-s-ethereum crypto-logo" },
+  { name: "Bitcoin Cash", api: "https://api.coinmarketcap.com/v1/ticker/bitcoin-cash/", site: "https://coinmarketcap.com/currencies/bitcoin-cash/", className: "s-s-bitcoin-cash crypto-logo" },
+  { name: "Ripple", api: "https://api.coinmarketcap.com/v1/ticker/ripple/", site: "https://coinmarketcap.com/currencies/ripple/", className: "s-s-ripple crypto-logo" },
+  { name: "Litecoin", api: "https://api.coinmarketcap.com/v1/ticker/litecoin/", site: "https://coinmarketcap.com/currencies/litecoin/", className: "s-s-litecoin crypto-logo" },
+  { name: "Dash", api: "https://api.coinmarketcap.com/v1/ticker/dash/", site: "https://coinmarketcap.com/currencies/dash/", className: "s-s-dash crypto-logo" },
+  { name: "NEO", api: "https://api.coinmarketcap.com/v1/ticker/neo/", site: "https://coinmarketcap.com/currencies/neo/", className: "s-s-neo crypto-logo" },
+  { name: "NEM", api: "https://api.coinmarketcap.com/v1/ticker/nem/", site: "https://coinmarketcap.com/currencies/nem/", className: "s-s-nem crypto-logo" },
+  { name: "Monero", api: "https://api.coinmarketcap.com/v1/ticker/monero/", site: "https://coinmarketcap.com/currencies/monero/", className: "s-s-monero crypto-logo" },
+  { name: "Ethereum Classic", api: "https://api.coinmarketcap.com/v1/ticker/ethereum-classic/", site: "https://coinmarketcap.com/currencies/ethereum-classic/", className: "s-s-ethereum-classic crypto-logo" }
 ];
